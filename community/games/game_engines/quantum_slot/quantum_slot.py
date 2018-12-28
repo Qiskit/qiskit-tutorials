b73a469f1b23bfe2a5a00f1a13b216d1326be829
Paul Nation <nonhermitian@gmail.com>
2018-12-28 10:55:47 -0500

    update slot machine

--- community/games/game_engines/quantum_slot/quantum_slot.py
+++ community/games/game_engines/quantum_slot/quantum_slot.py
@@ -5,6 +5,7 @@ from qiskit import *
 from qiskit.tools.monitor import job_monitor
 import ipywidgets as widgets
 from IPython.display import display
+import threading
 import os
 script_dir = os.path.dirname(__file__)
 
@@ -19,12 +20,13 @@ def quantum_slot_machine():
     qslot = widgets.VBox(children=[slot, opts])
     qslot.children[0].children[1].children[7].children[1]._qslot = qslot
     qslot.children[0].children[1].children[7].children[1].on_click(pull_slot)
+    ibmq_thread = threading.Thread(target=get_ibmq_ints, args=(qslot,))
+    ibmq_thread.start()
     display(qslot)
 
 
 def get_slot_values(backend, qslot):
-    qslot.children[1].children[1].clear_output()
-    if backend == 'Simulator':
+    if backend == 'qasm_simulator':
         back = BasicAer.get_backend('qasm_simulator')
         q = QuantumRegister(9, name='q')
         c = ClassicalRegister(9, name='c')
@@ -37,22 +39,12 @@ def get_slot_values(backend, qslot):
         counts = list(result.get_counts().keys())[0]
         return int(counts[0:3], 2), int(counts[3:6], 2), int(counts[6:9], 2)
     elif backend == 'ibmq_5_tenerife':
-        back = IBMQ.get_backend('ibmq_5_tenerife')
-        if len(qslot.children[0]._stored_ints) < 3:
-            q = QuantumRegister(3, name='q')
-            c = ClassicalRegister(3, name='c')
-            qc = QuantumCircuit(q, c)
-            for kk in range(3):
-                qc.h(q[kk])
-            qc.measure(q, c)
-            job = execute(qc, backend=back, shots=300, memory=True)
-            with qslot.children[1].children[1]:
-                job_monitor(job)
-            qslot.children[0]._stored_ints = [
-                int(kk, 16) for kk in job.result().results[0].data.memory]
         int1 = qslot.children[0]._stored_ints.pop(0)
         int2 = qslot.children[0]._stored_ints.pop(0)
         int3 = qslot.children[0]._stored_ints.pop(0)
+        if len(qslot.children[0]._stored_ints) == 0:
+            ibmq_thread = threading.Thread(target=get_ibmq_ints, args=(qslot,))
+            ibmq_thread.start()
         return int1, int2, int3
 
     elif backend == 'ANU QRNG':
@@ -76,15 +68,89 @@ def set_images(a, b, c, qslot):
     slot1.value = qslot.children[0]._images[b]
     slot2.value = qslot.children[0]._images[c]
 
+def update_credits(change, qslot):
+    qslot.children[0]._credits += change
+    qslot.children[1].children[1].value = front_str + \
+        str(qslot.children[0]._credits)+back_str
+
+def compute_payout(ints, qslot):
+    out = 1
+    #Paytable
+    # all sevens
+    if all([x == 7 for x in ints]):
+        value = 700
+    # all watermelons
+    elif all([x == 6 for x in ints]):
+        value = 200
+    # all strawberry
+    elif all([x == 5 for x in ints]):
+        value = 10
+    # all orange
+    elif all([x == 4 for x in ints]):
+        value = 20
+    # all lemon
+    elif all([x == 3 for x in ints]):
+        value = 60
+    # all grape
+    elif all([x == 2 for x in ints]):
+        value = 15
+    # all cherry
+    elif all([x == 1 for x in ints]):
+        value = 40
+    # all bell
+    elif all([x == 0 for x in ints]):
+        value = 80
+    # two bells
+    elif sum([x == 0 for x in ints])== 2:
+        value = 5
+    # two bells
+    elif sum([x == 0 for x in ints]) == 1:
+        value = 1
+    else:
+        value = 0
+    if value:
+        update_credits(value, qslot)
+    
+    # if no credits left
+    if qslot.children[0]._credits <= 0:
+        qslot.children[1].children[1].value = front_str + \
+            "LOSE"+back_str
+        out = 0
+    return out
+
 
 def pull_slot(b):
-    b.disabled = True
     qslot = b._qslot
+    update_credits(-1, qslot)
+    b.disabled = True
     set_images(-1, -1, -1, qslot)
     backend = qslot.children[1].children[0].value
     ints = get_slot_values(backend, qslot)
     set_images(ints[0], ints[1], ints[2], qslot)
-    b.disabled = False
+    alive = compute_payout(ints, qslot)
+    if alive:
+        b.disabled = False
+
+# generate new ibm q values
+def get_ibmq_ints(qslot):
+    qslot.children[1].children[0].options = ['qasm_simulator', 'ANU QRNG']
+    qslot.children[1].children[0].value = 'qasm_simulator'
+    back = IBMQ.get_backend('ibmq_5_tenerife')
+    q = QuantumRegister(3, name='q')
+    c = ClassicalRegister(3, name='c')
+    qc = QuantumCircuit(q, c)
+    for kk in range(3):
+        qc.h(q[kk])
+    qc.measure(q, c)
+    job = execute(qc, backend=back, shots=300, memory=True)
+    qslot.children[1].children[2].clear_output()
+    with qslot.children[1].children[2]:
+        job_monitor(job)
+    qslot.children[0]._stored_ints = [
+        int(kk, 16) for kk in job.result().results[0].data.memory]
+
+    qslot.children[1].children[0].options = ['qasm_simulator', 'ibmq_5_tenerife', 'ANU QRNG']
+
 
 #top
 top_file = open(script_dir+"/machine/slot_top.png", "rb")
@@ -220,15 +286,30 @@ slot._images = {}
 for kk, img in enumerate(imgs):
     slot._images[kk-1] = open(script_dir+"/symbols/%s" % img, "rb").read()
 
-gen = widgets.ToggleButtons(
-    options=['Simulator', 'ibmq_5_tenerife', 'ANU QRNG'],
+slot._credits = 20
+
+solver = widgets.Dropdown(
+    options=['qasm_simulator', 'ibmq_5_tenerife', 'ANU QRNG'],
+    value='qasm_simulator',
     description='',
     disabled=False,
-    button_style=''
+    layout=widgets.Layout(width='25%', padding='10px')
 )
 
-out = widgets.Output()
+front_str = "<div style = 'background-color:#000000; height:70px; text-align:right;padding:10px' > <p style='color:#FFFFFF; font-size: 60px;margin: 10px'>"
+back_str = "</p></div>"
 
-opts = widgets.HBox(children=[gen, out],
-                    layout=widgets.Layout(border='1px solid black'))
+payout = widgets.HTML(
+    value=front_str+str(20)+back_str,
+    placeholder='',
+    description='',
+    layout=widgets.Layout(width='33%', height='70px')
+)
+
+out = widgets.Output(layout=widgets.Layout(width='33%', padding='10px'))
+
+opts = widgets.HBox(children=[solver, payout, out],
+                    layout=widgets.Layout(width='100%',
+                                         justify_content='center',
+                                         border='2px solid black'))
 
