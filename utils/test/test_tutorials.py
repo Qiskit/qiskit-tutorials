6e2914c70735b9aafaabfc46e890ac42351a1aea
Matthew Treinish <mtreinish@kortar.org>
2019-08-19 15:29:46 -0400

    Make tutorial tests run all notebooks by default and configurable
    This commit updates the often forgotten about test module to actually be
    runnable by default. It also makes the hard coded values for path,
    kernel, and timeout configurable via env variables so people can adjust
    these easily without having to hand edit code. The exlusion list option
    was not added because doing that in a test module is redundant a decent
    test runner will support test selection in a more robust manner and that
    should be leveraged instead of exclusion list.
    
    In addition because the notebook execution can be slow this commit also
    adds a .stestr.conf file so that we can run the notebooks in parallel
    (just like we do for the terra unit tests). This can be done with
    'stestr run' and by default it will run as many processes as you have
    cpus to run a notebook each (assuming you have stestr pip installed
    along with the notebook dependencies).

--- utils/test/test_tutorials.py
+++ utils/test/test_tutorials.py
@@ -52,11 +52,11 @@ from nbconvert.preprocessors import ExecutePreprocessor
 # List of manual exclusion (for example, ["reference/foo/problematic.ipynb"]).
 EXCLUDED_NOTEBOOKS = []
 # Timeout (in seconds) for a single notebook.
-TIMEOUT = 6000
+TIMEOUT = os.getenv('TIMEOUT', 6000)
 # Jupyter kernel to execute the notebook in.
-JUPYTER_KERNEL = 'python3'
+JUPYTER_KERNEL = os.getenv('JUPYTER_KERNEL', 'python3')
 # Glob expression for discovering the notebooks.
-NOTEBOOK_PATH = 'reference/**/*.ipynb'
+NOTEBOOK_PATH = os.getenv('NOTEBOOK_PATH', 'qiskit/**/*.ipynb')
 
 
 # Retrieve the notebooks recursively.
